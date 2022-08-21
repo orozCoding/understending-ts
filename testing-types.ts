1cb965430bb3f339ae21088f1c226b9b152da69c
orozCoding <orozcoding@gmail.com>
2022-08-21 19:38:47 -0400

    Create enums

--- testing-types.ts
+++ testing-types.ts
@@ -9,25 +9,32 @@
 //   hobbies: ["Sports", "Cooking"],
 //   role: [2, "author"],
 // };
+
 // let favoriteActivities: string[];
 // favoriteActivities = ["Sports"];
+
 // console.log(person.name);
+
 // for (const hobby of person.hobbies) {
 //   console.log(hobby.toUpperCase());
 // }
+
 // person.role.push("admin");
 // person.role[1] = 10;
+
 // person.role = [0, "admin", "user"];
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 5] = "ADMIN";
-    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 7] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Maximilian",
-    age: 5,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.ADMIN
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "Maximilian",
+  age: 5,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 };
+
 console.log(person.role);
