361329aab3e84378e4cc37b03c93eefc7421ffa9
orozCoding <orozcoding@gmail.com>
2022-08-21 19:21:18 -0400

    Add new types and create possible errors

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-function add(n1, n2) {
-    return n1 + n2;
+var person = {
+    name: "Maximilian",
+    age: 5,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
+};
+var favoriteActivities;
+favoriteActivities = ["Sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
--- app.ts
+++ app.ts
@@ -1,10 +1,25 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Maximilian",
+  age: 5,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
 }
 
-const number1 = 5;
-const number2 = 2.8;
+// person.role.push("admin");
+// person.role[1] = 10;
 
-const result = add(number1, number2);
-
-console.log(result);
+// person.role = [0, "admin", "user"];
